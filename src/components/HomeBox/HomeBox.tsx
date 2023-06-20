189509e1167ede595f5bf4fcf4f057d382dc4a3f
namtit2932k <namtit2932k@gmail.com>
2023-06-20 22:26:53 +0700

    home login register pages

---
+++ src/components/HomeBox/HomeBox.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+import styles from './HomeBox.module.css'
+
+interface Props {
+    image: string
+    topText: string
+    title: string
+    text: string
+    btn1: string
+    btn2: string
+}
+
+const HomeBox: FC<Props> = ({ image, topText, title, text, btn1, btn2 }) => {
+    return (
+        <div className={`${styles.homeBox} w-full text-center`}>
+            <img src={image} alt='noImg' className='w-11/12 mx-auto mb-10' />
+            {topText !== '' && <p>{topText}</p>}
+            <h1 className={styles.title}>{title}</h1>
+            <p className='mt-5'>{text}</p>
+            <div className={`${styles.btnBox}`}>
+                <button className={styles.btn}>{btn1}</button>
+                {btn2 !== '' && <button className={styles.btn}>{btn2}</button>}
+            </div>
+        </div>
+    )
+}
+
+export default HomeBox
--- src/pages/FrontEnd/Home/Home.tsx
+++ src/pages/FrontEnd/Home/Home.tsx
@@ -1,5 +1,65 @@
+import HomeBox from '../../../components/HomeBox/HomeBox'
+import HomeSlide from '../../../components/HomeSlide/HomeSlide'
+
 function Home() {
-    return <h2>Home page</h2>
+    const fakeData = [
+        {
+            image: 'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/w_467,c_limit/6c6bdc2e-abbb-4b38-b57d-0a0d557b3f3f/yoga-dri-fit-luxe-cropped-tank-xvfQ34.png',
+            title: 'Nike Yoga Dri-FIT Luxe',
+            description: 'Women Cropped Tank',
+            price: '$49.99'
+        },
+        {
+            image: 'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/w_467,c_limit/6c6bdc2e-abbb-4b38-b57d-0a0d557b3f3f/yoga-dri-fit-luxe-cropped-tank-xvfQ34.png',
+            title: 'Nike Yoga Dri-FIT Luxe 2',
+            description: 'Women Cropped Tank',
+            price: '$49.99'
+        },
+        {
+            image: 'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/w_467,c_limit/6c6bdc2e-abbb-4b38-b57d-0a0d557b3f3f/yoga-dri-fit-luxe-cropped-tank-xvfQ34.png',
+            title: 'Nike Yoga Dri-FIT Luxe 3',
+            description: 'Women Cropped Tank',
+            price: '$49.99'
+        },
+        {
+            image: 'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/w_467,c_limit/6c6bdc2e-abbb-4b38-b57d-0a0d557b3f3f/yoga-dri-fit-luxe-cropped-tank-xvfQ34.png',
+            title: 'Nike Yoga Dri-FIT Luxe 4',
+            description: 'Women Cropped Tank',
+            price: '$49.99'
+        },
+        {
+            image: 'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.3/w_467,c_limit/6c6bdc2e-abbb-4b38-b57d-0a0d557b3f3f/yoga-dri-fit-luxe-cropped-tank-xvfQ34.png',
+            title: 'Nike Yoga Dri-FIT Luxe 5',
+            description: 'Women Cropped Tank',
+            price: '$49.99'
+        }
+    ]
+
+    return (
+        <div className='home'>
+            <HomeBox
+                image='https://i.pinimg.com/originals/96/34/ef/9634ef33aceefe26474bf1e95812a126.gif'
+                topText='Summer Kicks'
+                title='STEP INTO SUMMER STYLE'
+                text='For days when you need fresh kicks that can keep up with your every move. #ChaseTheDays'
+                btn1='Shop Kicks'
+                btn2='Shop Summer Styles'
+            />
+
+            <HomeSlide heading='Find Your Flow Fits' data={fakeData} />
+
+            <HomeBox
+                image='https://wearecollins.imgix.net/uploads/3%20Collins%20x%20Air%20Max.gif?auto=format%2Ccompress&dpr=2.63&fit=max&q=90&w=400'
+                topText=''
+                title='LIVERPOOL 2023/24 HOME'
+                text='An iconic design made modern, the Reds walk on as one.'
+                btn1='Shop'
+                btn2=''
+            />
+
+            <HomeSlide heading='Find Your Flow Fits' data={fakeData} />
+        </div>
+    )
 }
 
 export default Home
