bd191c55d6684a5e43aad9cab219203bf37cc935
PhamBaVan <bavanpham2000@gmail.com>
2023-06-21 17:47:18 +0700

    updated cart from branch main 17:47 21/06/2023

--- src/pages/FrontEnd/Cart/Cart.tsx
+++ src/pages/FrontEnd/Cart/Cart.tsx
@@ -12,7 +12,7 @@ function Cart() {
                             alt=''
                         />
                     </div>
-                    <div className='mx-[1.4rem] flex'>
+                    <div className='mx-[1.4rem] flex w-full justify-between'>
                         <div>
                             <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Force 1 '07 LX</h3>
                             <p className='mb-[0.4rem]'>Men's Shoes</p>
@@ -48,15 +48,15 @@ function Cart() {
                     <div className='w-[200px]'>
                         <img
                             className='w-full object-cover'
-                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f53c9996-2b71-4af5-9817-01c010dc79f6/air-force-1-07-lx-shoes-QKdcNj.png'
+                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/861b306f-bc46-402a-8b4a-0c1aaa97356c/blazer-low-77-jumbo-shoes-kl0MCq.png'
                             alt=''
                         />
                     </div>
-                    <div className='mx-[1.4rem] flex'>
+                    <div className='mx-[1.4rem] flex w-full justify-between'>
                         <div>
-                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Force 1 '07 LX</h3>
+                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Blazer Low '77 Jumbo</h3>
                             <p className='mb-[0.4rem]'>Men's Shoes</p>
-                            <p className='mb-[0.4rem]'>White/Beach/White/Smoke Grey</p>
+                            <p className='mb-[0.4rem]'>White/Photon Dust/Light Smoke Grey/University Red</p>
                             <div>
                                 <label htmlFor='' className='me-[5px]'>
                                     Size
@@ -88,15 +88,15 @@ function Cart() {
                     <div className='w-[200px]'>
                         <img
                             className='w-full object-cover'
-                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f53c9996-2b71-4af5-9817-01c010dc79f6/air-force-1-07-lx-shoes-QKdcNj.png'
+                            src='https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f9e940d3-2192-434e-a017-072303ce2f14/air-max-90-shoes-N7Tbw0.png'
                             alt=''
                         />
                     </div>
-                    <div className='mx-[1.4rem] flex'>
+                    <div className='mx-[1.4rem] flex w-full justify-between'>
                         <div>
-                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Force 1 '07 LX</h3>
+                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Max 90</h3>
                             <p className='mb-[0.4rem]'>Men's Shoes</p>
-                            <p className='mb-[0.4rem]'>White/Beach/White/Smoke Grey</p>
+                            <p className='mb-[0.4rem]'>Photon Dust/Metallic Silver/Black/University Red</p>
                             <div>
                                 <label htmlFor='' className='me-[5px]'>
                                     Size
@@ -128,15 +128,15 @@ function Cart() {
                     <div className='w-[200px]'>
                         <img
                             className='w-full object-cover'
-                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f53c9996-2b71-4af5-9817-01c010dc79f6/air-force-1-07-lx-shoes-QKdcNj.png'
+                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/eac5f1f7-36c3-4ce7-8bf2-57f44c2dd576/air-flight-lite-mid-shoes-wNrQDg.png'
                             alt=''
                         />
                     </div>
-                    <div className='mx-[1.4rem] flex'>
+                    <div className='mx-[1.4rem] flex w-full justify-between'>
                         <div>
-                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Force 1 '07 LX</h3>
+                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Flight Lite Mid</h3>
                             <p className='mb-[0.4rem]'>Men's Shoes</p>
-                            <p className='mb-[0.4rem]'>White/Beach/White/Smoke Grey</p>
+                            <p className='mb-[0.4rem]'>Black/Varsity Red/White</p>
                             <div>
                                 <label htmlFor='' className='me-[5px]'>
                                     Size
@@ -168,15 +168,15 @@ function Cart() {
                     <div className='w-[200px]'>
                         <img
                             className='w-full object-cover'
-                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f53c9996-2b71-4af5-9817-01c010dc79f6/air-force-1-07-lx-shoes-QKdcNj.png'
+                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/0d265602-b5fa-46c1-8eb6-b3e56c7f52b9/air-jordan-1-mid-shoes-SQf7DM.png'
                             alt=''
                         />
                     </div>
-                    <div className='mx-[1.4rem] flex'>
+                    <div className='mx-[1.4rem] flex w-full justify-between'>
                         <div>
-                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Force 1 '07 LX</h3>
+                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Air Jordan 1 Mid</h3>
                             <p className='mb-[0.4rem]'>Men's Shoes</p>
-                            <p className='mb-[0.4rem]'>White/Beach/White/Smoke Grey</p>
+                            <p className='mb-[0.4rem]'>University Blue/White/Black</p>
                             <div>
                                 <label htmlFor='' className='me-[5px]'>
                                     Size
@@ -208,15 +208,15 @@ function Cart() {
                     <div className='w-[200px]'>
                         <img
                             className='w-full object-cover'
-                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f53c9996-2b71-4af5-9817-01c010dc79f6/air-force-1-07-lx-shoes-QKdcNj.png'
+                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f21ce99a-a0f1-410f-a2ff-c93b27f457b2/react-phantom-run-flyknit-2-road-running-shoes-LcSGB7.png'
                             alt=''
                         />
                     </div>
-                    <div className='mx-[1.4rem] flex'>
+                    <div className='mx-[1.4rem] flex w-full justify-between'>
                         <div>
-                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Force 1 '07 LX</h3>
-                            <p className='mb-[0.4rem]'>Men's Shoes</p>
-                            <p className='mb-[0.4rem]'>White/Beach/White/Smoke Grey</p>
+                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike React Phantom Run Flyknit 2</h3>
+                            <p className='mb-[0.4rem]'>Men's Road Running Shoes</p>
+                            <p className='mb-[0.4rem]'>Black/White</p>
                             <div>
                                 <label htmlFor='' className='me-[5px]'>
                                     Size
@@ -248,15 +248,15 @@ function Cart() {
                     <div className='w-[200px]'>
                         <img
                             className='w-full object-cover'
-                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f53c9996-2b71-4af5-9817-01c010dc79f6/air-force-1-07-lx-shoes-QKdcNj.png'
+                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/bdbfc393-1239-4fdc-a143-b0759c789c6a/jordan-stadium-90-shoes-Jn6ZH4.png'
                             alt=''
                         />
                     </div>
-                    <div className='mx-[1.4rem] flex'>
+                    <div className='mx-[1.4rem] flex w-full justify-between'>
                         <div>
-                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Force 1 '07 LX</h3>
+                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Jordan Stadium 90</h3>
                             <p className='mb-[0.4rem]'>Men's Shoes</p>
-                            <p className='mb-[0.4rem]'>White/Beach/White/Smoke Grey</p>
+                            <p className='mb-[0.4rem]'>Brown/Black/University Red/Sand Drift</p>
                             <div>
                                 <label htmlFor='' className='me-[5px]'>
                                     Size
@@ -288,15 +288,15 @@ function Cart() {
                     <div className='w-[200px]'>
                         <img
                             className='w-full object-cover'
-                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f53c9996-2b71-4af5-9817-01c010dc79f6/air-force-1-07-lx-shoes-QKdcNj.png'
+                            src='https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f6bb75e3-f86b-4031-9265-4343b1e14bd7/jumpman-two-trey-shoes-rhmBzG.png'
                             alt=''
                         />
                     </div>
-                    <div className='mx-[1.4rem] flex'>
+                    <div className='mx-[1.4rem] flex w-full justify-between'>
                         <div>
-                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Nike Air Force 1 '07 LX</h3>
+                            <h3 className={`mb-[0.4rem] ${styles.h3Heading}`}>Jumpman Two Trey</h3>
                             <p className='mb-[0.4rem]'>Men's Shoes</p>
-                            <p className='mb-[0.4rem]'>White/Beach/White/Smoke Grey</p>
+                            <p className='mb-[0.4rem]'>Black/Dark Concord/White/True Red</p>
                             <div>
                                 <label htmlFor='' className='me-[5px]'>
                                     Size
