aefbadb0fc4efd62013298764d879ee329da427c
namtit2932k <namtit2932k@gmail.com>
2023-06-24 20:38:58 +0700

    redux store fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import authSlice from './authSlice'
+import cartSlice from './features/cartSlice'
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import { useDispatch } from 'react-redux'
@@ -11,7 +12,8 @@ const persistConfig = {
 }
 
 const rootReducer = combineReducers({
-    auth: authSlice
+    auth: authSlice,
+    cart: cartSlice
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
