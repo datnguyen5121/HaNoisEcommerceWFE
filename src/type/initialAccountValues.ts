76189edf1efb5b0cd8d3fb8808499b87de60ff7e
PhamBaVan <bavanpham2000@gmail.com>
2023-06-29 14:15:27 +0700

    updated initialAccountValues main from VanPB2 14:15 29/06/2023

---
+++ src/type/initialAccountValues.ts
@@ -0,0 +1,11 @@
+import { AccountValues } from './AccountValues'
+export const initialAccountValues: AccountValues = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    address: '',
+    gender: '',
+    phone: '',
+    roleId: ''
+}
